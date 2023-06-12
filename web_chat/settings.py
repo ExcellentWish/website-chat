51b0dc37a4f778e8b5dee2850151e0163988823e
Nigel Doheny <nigel.doheny93@gmail.com>
2023-06-12 17:05:14 +0000

    Amend settings

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'web_chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DEVELOPMENT' = 'True':
+if 'DEVELOPMENT' == 'True':
 
     DATABASES = {
         'default': {
