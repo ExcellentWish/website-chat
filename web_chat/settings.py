96b9c107b69d652a27ca0df03637edc73bf39751
Nigel Doheny <96533405+ExcellentWish@users.noreply.github.com>
2023-06-13 13:11:20 +0000

    amend settings

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -61,10 +61,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web_chat.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR. 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
