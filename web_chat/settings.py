7c31f8d4b2f3d2f0344e511bed708be950fd8d4a
Nigel Doheny <96533405+ExcellentWish@users.noreply.github.com>
2023-06-13 14:14:12 +0000

    Update setting for static

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'web_chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DEVELOPMENT' == 'True':
+# if 'DEVELOPMENT' == 'True':
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-    'default':
-    dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+DATABASES = {
+'default':
+dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
